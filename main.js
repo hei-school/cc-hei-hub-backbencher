d2fa2714959ec34326f46efcbcae11ddf2144f0f
kenny <hei.kenny.3@gmail.com>
2023-12-13 17:01:26 +0300

    feat : review and fix with linter

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-const prompt = require("prompt-sync")();
-const { addError } = require('./error-100-400');
-const fs = require('fs');
+const prompt = require('prompt-sync')()
+const { addError } = require('./error-100-400')
+const fs = require('fs')
 
-let tableau = [];
+let tableau = []
 
 // Charger le tableau depuis le fichier s'il existe
 try {
-  const data = fs.readFileSync('tableau.json', 'utf8');
-  tableau = JSON.parse(data);
+  const data = fs.readFileSync('tableau.json', 'utf8')
+  tableau = JSON.parse(data)
 } catch (err) {
   // Le fichier n'existe probablement pas encore, c'est normal
 }
 
-function sauvegarderTableau() {
+function sauvegarderTableau () {
   // Sauvegarder le tableau dans le fichier
-  fs.writeFileSync('tableau.json', JSON.stringify(tableau));
+  fs.writeFileSync('tableau.json', JSON.stringify(tableau))
 }
 
-function ajouterNombre() {
-  const nombre = prompt('Entrez un nombre de 1 à 10 : ');
+function ajouterNombre () {
+  const nombre = prompt('Entrez un nombre de 1 à 10 : ')
 
   // Gestion des erreurs
   try {
-    addError(tableau, nombre);
+    addError(tableau, nombre)
     // Ajouter l'élément au tableau uniquement si aucune erreur n'est détectée
-    tableau.push(nombre);
-    console.log("Tableau", [...tableau]);
-    sauvegarderTableau();
+    tableau.push(nombre)
+    console.log('Tableau', [...tableau])
+    sauvegarderTableau()
   } catch (error) {
-    console.log(error.message);
+    console.log(error.message)
   }
 }
 
-ajouterNombre();
+ajouterNombre()
