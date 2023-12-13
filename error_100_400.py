3b4d0bc3ce47bea7860afd115a2448a2dfbbf2f3
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2023-12-13 18:52:26 +0300

    Update: handling errors 423 to 451

--- error_100_400.py
+++ error_100_400.py
@@ -8,6 +8,10 @@ def add_error(tableau, nombre):
         raise ValueError("Erreur 100 : Nombre dupliqué")
     elif int_value == 666:
         raise ValueError("Erreur 400 : Nombre sensible")
+    elif int(nombre) == 69:
+        raise ValueError("Erreur 451 : Action unavailable due to Legal Reasons")
+    elif int(nombre) == 8:
+        raise ValueError("Erreur 423 : Lock exception")
     elif not (1 <= int_value <= 10) and (int_value != 0) and (int_value > 10):
         raise ValueError("Erreur 400 : Mauvais type de nombre")
     elif not (1 <= int_value <= 10) and (int_value != 0) and (int_value > 10):
