c46942a1dbd2f09121c350fcc2ed06c3e43511d8
ezdev2 <fansoaezra@gmail.com>
2023-12-13 14:29:46 +0300

    update main.py & feat : add error_100_400.py

---
+++ error_100_400.py
@@ -0,0 +1,12 @@
+def add_error(tableau, nombre):
+    try:
+        int_value = int(nombre)
+    except ValueError:
+        raise ValueError("Erreur 400 : Mauvais type de nombre")
+
+    if nombre in tableau:
+        raise ValueError("Erreur 100 : Nombre dupliqué")
+    elif int_value == 666:
+        raise ValueError("Erreur 400 : Nombre sensible")
+    elif not 1 <= int_value <= 10:
+        raise ValueError("Erreur 400 : Nombre invalide")
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-# here code for pyhton 
+import json
+from error_100_400 import add_error
+
+try:
+    with open('tableau.json', 'r') as file:
+        tableau = json.load(file)
+except FileNotFoundError:
+    # Le fichier n'existe probablement pas encore, c'est normal
+    tableau = []
+
+def sauvegarder_tableau():
+    # Sauvegarder le tableau dans le fichier
+    with open('tableau.json', 'w') as file:
+        json.dump(tableau, file)
+
+def ajouter_nombre():
+    nombre = input('Entrez un nombre de 1 à 10 : ')
+
+    # Gestion des erreurs
+    try:
+        add_error(tableau, nombre)
+        # Ajouter l'élément au tableau uniquement si aucune erreur n'est détectée
+        tableau.append(nombre)
+        print("Tableau", tableau.copy())
+        sauvegarder_tableau()
+    except Exception as error:
+        print(error)
+
+ajouter_nombre()
